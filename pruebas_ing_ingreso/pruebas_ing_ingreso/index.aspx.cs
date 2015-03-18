8f76f4b1c9d176be76bf04face4c885cdca050d6
katherinsanta <dirsoporte@zencillo.com>
2015-03-17 19:15:27 -0500

    creacion de aplicativo web para el aprendisaje

---
+++ pruebas_ing_ingreso/pruebas_ing_ingreso/index.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pruebas_ing_ingreso
+{
+    public partial class _in : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
